67ac248d7883fd1018ad5aefada970444c89c494
mandy0529 <59499779+mandy0529@users.noreply.github.com>
2020-11-19 14:48:52 +1100

    nike html  완성

---
+++ img/seref collection_files/featuresManifest.js
@@ -0,0 +1,64 @@
+window.__featureManifest = {
+  features: {
+    'acc-consolidatedReturnsMessage': false,
+    'acc-int-vouchers': true,
+    'web-plp-new-facets': [],
+    'pay-klarnaInstalments': true,
+    'plp-show-discount-label': true,
+    'chk-refer-a-friend': true,
+    'chk-raf-lookup': true,
+    'chk-mention-me-banner': true,
+    'acc-clearpay': true,
+    'acc-klarnapayin3': true,
+    'chk-klarna-au': true,
+    'acc-afterpay-us': true,
+    'chk-afterpay-us': true,
+    'chk-validate-postcode': true,
+    'chk-paypalpayin3': true,
+    'acc-paypalpayin3': true,
+    'chk-arvato-afterpay': true,
+    'acc-arvato-afterpay': true,
+    'pdp-btl-from-search': false,
+    'pdp-react-might-like': true,
+    'pdp-ratings-and-reviews': true,
+    'pdp-ratings-and-reviews-phase-2': true,
+    'pdp-paymentOptions-gb': {
+      value: ['payPalPayIn3']
+    },
+    'pdp-paymentOptions-au': {
+      value: ['klarnaPI4', 'afterpay']
+    },
+    'pdp-paymentOptions-nz': {
+      value: ['afterpay']
+    },
+    't1288-web-3ds-card-payment': {
+      active: true
+    },
+    'web-saved-lists-reordering-animation': {
+      enabled: true
+    },
+    'chk-validate-address': true,
+    'chk-forter-poc': true,
+    'pdp-fit-assistant-logging': {
+      probability: 5
+    },
+    noReturnsNote: true,
+    'acc-returnsEnabledCountries': {
+      criteria: {
+        includes: {
+          browseCountry: ['AT', 'AU', 'BE', 'CC', 'CH', 'CX', 'CZ', 'DE', 'DK', 'FR', 'GB', 'HK', 'IT', 'NF', 'NL', 'NZ', 'PL', 'PT', 'SE', 'US']
+        }
+      }
+    },
+    'all-qualifies-for-free-returns-feature': {
+      criteria: {
+        includes: {
+          browseCountry: ['AU', 'AT', 'BE', 'BG', 'HR', 'CY', 'CZ', 'DK', 'EE', 'FI', 'FR', 'DE', 'GR', 'HK', 'HU', 'IE', 'IT', 'LV', 'LT', 'LU', 'MT', 'NL', 'NZ', 'PL', 'PT', 'RO', 'RU', 'SA', 'SG', 'SK', 'SI', 'ES', 'SE', 'CH', 'AE', 'GB', 'US']
+        }
+      }
+    }
+  },
+  mvt: {
+    'web-plp-show-more-filters-button': {}
+  }
+};
